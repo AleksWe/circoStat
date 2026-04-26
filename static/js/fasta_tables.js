0f366da7e5cddeb582c289aa3ca0ec24ba580037
AleksWe <aleksandra.weiss28@gmail.com>
2026-04-26 17:31:28 +0200

    frontend changes - .csv autofill group and alignment box added; banner update

--- static/js/fasta_tables.js
+++ static/js/fasta_tables.js
@@ -2,20 +2,74 @@ const fileInput = document.getElementById("fileInput");
 const fastaTable = document.getElementById("fastaTable");
 
 fileInput.addEventListener("change", (e) => {
-  console.log("Fasta files loaded. Saving info to form table...");
 
   console.log("files:", fileInput.files);
 
   fastaTable.innerHTML = "";
 
+  let isPremadeTable = 0;
+
   for (const file of fileInput.files) {
-    const row = document.createElement("tr");
+    // TODO: xslx and xls handling
+    // if (file.name.includes('.xlsx') || file.name.includes('.xls')){
+    //  isPremadeTable++;
+    //  break
+    //}
+    if (file.name.includes('.csv')){
+      isPremadeTable++;
 
-    row.innerHTML = `
-      <td>${file.name}</td>
-      <td><input></td>
-    `;
+      file.text().then(text => {
+        const rows = text.split(/\r?\n/);
+        console.log(rows);
 
-    fastaTable.appendChild(row);
+        rows.forEach(line => {
+          if (!line.trim()) return;
+
+          const { left, right } = splitLine(line);
+
+          const row = document.createElement("tr");
+          row.innerHTML = `
+                    <td>${left}</td>
+                    <td><input class="form-control form-control-sm" value="${right}"></td>
+                  `;
+
+          fastaTable.appendChild(row);
+        });
+      });
+      break
+    }
+  }
+
+  if(!isPremadeTable){
+    for (const file of fileInput.files) {
+
+      console.log(file.name)
+
+      if (file.name.includes('.fasta') || (file.name.includes('.fa'))) {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+          <td>${file.name}</td>
+          <td><input></td>
+        `;
+
+        fastaTable.appendChild(row);
+      }
+    }
   }
 });
+
+function splitLine(line) {
+  const match = line.match(/[,;]/); // finds first , or ;
+
+  if (!match) {
+    return { left: line.trim(), right: "" };
+  }
+
+  const sepIndex = match.index;
+
+  return {
+    left: line.slice(0, sepIndex).trim(),
+    right: line.slice(sepIndex + 1).trimStart()
+  };
+}
